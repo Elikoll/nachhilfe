29a8cbc270722aaa76e8547ad38695e520510945
Oglar <oglartist@gmail.com>
2020-06-13 19:52:25 +0200

    adds reversedarray

--- arrays/src/IntArrays.java
+++ arrays/src/IntArrays.java
@@ -1,5 +1,13 @@
 public class IntArrays {
 
+    /**
+     * 1. git add (or "git add -A .")
+     * 2. git commit -m "message in present tense"
+     * 3. git push
+     *
+     *
+     * @param args
+     */
     public static void main (String[] args){
 
         int[][] array = new int[][] {
@@ -17,6 +25,18 @@ public class IntArrays {
             totalArrayLength += array[i].length;
         }
         System.out.println("" + totalArrayLength);
+
+        // lesson 0:
+        // reverse the iteration.
+        // the array should be traversed from end to start.
+        // print "totalArrayLengthReversed" at the end.
+        // result should be the same as above.
+        int totalArrayLengthReversed = 0;
+        for (int i = array.length-1; i > -1; i--){
+            totalArrayLengthReversed += array[i].length;
+
+        }
+        System.out.println("" + totalArrayLengthReversed);
 
     }
 }
